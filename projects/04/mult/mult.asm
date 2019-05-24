6204419ea8eb015488aa5ecb938110c0c0eebd6d
Christian Bell <christiantcbell@gmail.com>
2019-05-24 17:36:52 +0100

    started multiplying - incrementing is working but the sum isn't being set

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -11,17 +11,43 @@
 // PSEUDO CODE:
   // R0 * R1 = R2
   // sum = 0
-  // for(i = 0, i < R0, i++)
+  // for(i = 1, i == R0, i++)
     // sum + R1
   // return sum
 
-@0
-D=A
-@sum
-M=D     // set sum to 0
-
-@0
+@1
 D=A
 @i
-M=D     // set i to 0
+M=D     // set i to 1
+
+(LOOP)
+  @i
+  D=M     // get i
+  @R0
+  D=M-D   // RO - i
+  @END
+  D;JEQ   // if i - RO == 0 END \ EXIT LOOP
+
+  @SUM
+  D=M     // get sum
+  @R1
+  M=D+M   // R1 + sum
+
+  @1
+  D=A
+  @i
+  M=M+D   // increment i
+
+  @LOOP
+  0;JMP   // go to the start of the loop
+
+(SUM)
+  @4
+  D=A
+  @R2
+  M=D     // set sum to 0
+
+(END)
+  @END
+  0;JMP
 
