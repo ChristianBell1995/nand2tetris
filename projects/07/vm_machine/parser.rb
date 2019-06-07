f19997582bf2ad3730496334b0efb54394e3cb7e
Christian Bell <christiantcbell@gmail.com>
2019-06-07 17:30:11 +0100

    added constant assembly and push

--- projects/07/vm_machine/parser.rb
+++ projects/07/vm_machine/parser.rb
@@ -16,14 +16,7 @@ class Parser
 
   end
 
-  SP = 'R0'
-  REGULAR_SEGMENTS = {'local' => 'LCL',
-                      'argument' => 'ARG',
-                      'this' => 'THIS',
-                      'that' => 'THAT'}.freeze
-
   def write_push_or_pop
-    p REGULAR_SEGMENTS.has_key?(@segment)
     case @segment
     when 'constant'
       handle_constant_segment
@@ -38,31 +31,47 @@ class Parser
     end
   end
 
-  POP = ["@SP",
-        "M=M-1",                             # Decrement the Stack Pointer
-        "D=M",                               # set D register to value of the SP
-        "@addr",
-        "A=M",                               # get the addr value from M
-        "M=D"]                               # set RAM[addr[m]] to D]
+  REGULAR_SEGMENTS = {'local' => 'LCL',
+                      'argument' => 'ARG',
+                      'this' => 'THIS',
+                      'that' => 'THAT'}.freeze
 
+  POP = ['@SP',
+        'M=M-1',                             # Decrement the Stack Pointer
+        'D=M',                               # set D register to value of the SP
+        '@addr',
+        'A=M',                               # get the addr value from M
+        'M=D']                               # set RAM[addr[m]] to D]
+
+  PUSH = ['@addr',
+          'D=M',                              # grab M of addr
+          '@SP',
+          'A=M',                              # grab M of SP
+          'M=D',                              # set the Memory of RAM[SP[M]] to D
+          '@SP',                              # increment SP
+          'M=M+1']
   def handle_regular_segments
-    if @command == 'pop'
-      # e.g. pop local 2
-      # addr = LCL + 2, SP--, *addr = *SP
-      ["@#{REGULAR_SEGMENTS[@segment]}",          # Find pointer for segment
-      "D=M+#{@num}",                              # Set D register to M + num
-      "@addr",
-      "M=D"] + POP                                # set temp variable address to D
-    else
-      # e.g. push local i
-      # addr = LCL + i, *SP = *addr, SP++
-
-    end
+    pop_or_push = @command == 'pop' ? POP : PUSH
+    # e.g. pop local 2
+    # addr = LCL + 2, SP--, *addr = *SP
+    # e.g. push local i
+    # addr = LCL + i, *SP = *addr, SP++
+    p ["//#{@line.join(' ')}",
+    "@#{REGULAR_SEGMENTS[@segment]}",          # Find pointer for segment
+    "D=M+#{@num}",                              # Set D register to M + num
+    '@addr',
+    'M=D'] + pop_or_push                        # set addr variable to D
   end
 
   def handle_constant_segment
     # e.g. push constant i
     # *SP = i, SP++
+    p ["//#{@line.join(' ')}",
+    "@SP",
+    "A=M",                        # set A register to SP[M]
+    "M=#{@num}",                  # set A[M] to num
+    "@SP",
+    "M=M+1"]                      # increment SP
   end
 
   def handle_static_segment
